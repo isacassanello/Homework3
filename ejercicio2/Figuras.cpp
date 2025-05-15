b07ddf600d630d4edf71b9ba59111d856ac6b7a0
Isabella Cassanello <icassanello@udesa.edu.ar>
2025-05-15 13:51:15 -0300

    arranque el 2

--- ejercicio2/Figuras.cpp
+++ ejercicio2/Figuras.cpp
@@ -1,2 +1,122 @@
 #include "Figuras.h"
 #include <iostream>
+using namespace std;
+
+// ===================================== IMPLEMENTACION CLASE PUNTO =====================================
+
+Punto::Punto(double x, double y) : coordenadaX(x), coordenadaY(y) {}
+
+double Punto::getX() const{
+    return coordenadaX;
+}
+
+double Punto::getY() const{
+    return coordenadaY;
+}
+
+double Punto::setX(double x){
+    this->coordenadaX = x;
+}
+
+double Punto::setY(double y){
+    this->coordenadaY = y;
+}
+
+void Punto::mostrar() const{
+    cout << "(" << coordenadaX << ", " << coordenadaY << ")" << endl;
+}  
+
+// ===================================== IMPLEMENTACION CLASE CIRCULO =====================================
+
+Circulo::Circulo(const Punto &centro, double r) : centro(centro), radio(r) {}
+
+Punto Circulo::getCentro(){
+    return centro;
+}
+double Circulo::getRadio(){
+    return radio;
+}
+
+double Circulo::setCentro(const Punto &c){
+    this->centro = c;
+}
+double Circulo::setRadio(double r){
+    this->radio = r;
+}
+
+void Circulo::mostrar() const{
+    cout << "Centro del circulo: "; centro.mostrar();
+    cout << "\nRadio del circulo: " << radio << endl;
+}
+
+
+// ===================================== IMPLEMENTACION CLASE ELIPSE =====================================
+
+Elipse::Elipse(const Punto &c, double a, double b) : centro(c), a(a), b(b) {}
+
+Punto Elipse::getCentro() const{
+    return centro;
+}
+
+double Elipse::getA() const{
+    return a;
+}
+
+double Elipse::getB() const{
+    return b; 
+}
+
+double Elipse::setCentro(const Punto &c){
+    this->centro = c;
+}
+
+double Elipse::setA(double a){
+    this->a = a;
+}
+
+double Elipse::setB(double b){
+    this->b = b;
+}
+
+void Elipse::mostrar() const{
+    cout << "Centro del Elipse: "; centro.mostrar();
+    cout << "\nSemieje mayor (a): " << a << "\nSemieje menor (b): " << b << endl;
+}
+
+
+// ===================================== IMPLEMENTACION CLASE CIRCULO =====================================
+
+Rectangulo::Rectangulo(const Punto &vertice, double a, double l) 
+    : verticeInferiorIzquierdo(vertice), ancho(a), largo(a) {}
+
+Punto Rectangulo::getVertice() const{
+    return verticeInferiorIzquierdo;
+}
+
+double Rectangulo::getAncho() const{
+    return ancho;
+}
+
+double Rectangulo::getLargo() const{
+    return largo;
+}
+
+double Rectangulo::setVertice(const Punto &vertice){
+    this->verticeInferiorIzquierdo = vertice;
+}
+
+double Rectangulo::setAncho(double a){
+    this->ancho = a;
+}
+
+double Rectangulo::setLargo(double l){
+    this->largo = l;
+}
+
+void Rectangulo::mostrar() const{
+    cout << "Vertice inferior izquierdo: "; verticeInferiorIzquierdo.mostrar();
+    cout << "\nAncho del rectangulo: " << ancho << "\nLargo del rectangulo: " << largo << endl;
+}
+
+
+// ===================================== IMPLEMENTACION CLASE PROCESADOR FIGURA =====================================
--- ejercicio2/Figuras.h
+++ ejercicio2/Figuras.h
@@ -1,9 +1,99 @@
 #pragma once
+#include <string> 
+using namespace std;
 
-class Punto{};
+// ===================================== CLASE PUNTO =====================================
 
-class Circulo{};
+class Punto{
+    private:
+        double coordenadaX;
+        double coordenadaY;
+    
+    public:
+        Punto() = default;
+        Punto(double x, double y);
 
-class Elipse{};
+        double getX() const;
+        double getY() const;
 
-class Rectangulo{};
+        double setX(double x);
+        double setY(double y);
+
+        void mostrar() const;     
+        
+        ~Punto() = default;
+};
+
+// ===================================== CLASE CIRCULO =====================================
+
+class Circulo{
+    private:
+        Punto centro;
+        double radio;
+    
+    public:
+        Circulo() = default;
+        Circulo(const Punto &centro, double r);
+
+        Punto getCentro();
+        double getRadio();
+
+        double setCentro(const Punto &c);
+        double setRadio(double r);
+
+        void mostrar() const;
+
+        ~Circulo() = default;
+};
+
+// ===================================== CLASE ELIPSE =====================================
+
+class Elipse{
+    private:
+        Punto centro;
+        double a; // semieje mayor 
+        double b; // semieje menor 
+    
+    public:
+        Elipse() = default;
+        Elipse(const Punto &centro, double a, double b);
+
+        Punto getCentro() const;
+        double getA() const;
+        double getB() const;
+
+        double setCentro(const Punto &c);
+        double setA(double a);
+        double setB(double b);
+
+        void mostrar() const;
+
+        ~Elipse() = default;
+};
+
+// ===================================== CLASE RECTANGULO =====================================
+
+class Rectangulo{
+    private:
+        Punto verticeInferiorIzquierdo; 
+        double ancho;
+        double largo;
+    
+    public:
+        Rectangulo() = default;
+        Rectangulo(const Punto &vertice, double a, double l);
+
+        Punto getVertice() const;
+        double getAncho() const;
+        double getLargo() const;
+
+        double setVertice(const Punto &vertice);
+        double setAncho(double a);
+        double setLargo(double l);
+
+        void mostrar() const;
+
+        ~Rectangulo() = default;
+};
+
+// ===================================== CLASE PROCESADOR FIGURA =====================================
