bc872475512a30054623277baaa927e175fbdd8e
Isabella Cassanello <icassanello@udesa.edu.ar>
2025-05-25 18:32:50 -0300

    modifique ej 3. tiene errores

--- ejercicio3/Clases.h
+++ ejercicio3/Clases.h
@@ -2,44 +2,81 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <utility>
+#include <type_traits>
 using namespace std;
 
 // =========================== Clase 1 ===========================
 
+template <typename T>
 class JuntarDatos{
     private:
-        using vecDoubles = pair<string, vector<double>>;
-        using palabras = pair<string, vector<string>>;
-        using listas = pair<string, vector<vector<int>>>;
-
-        vecDoubles doubles;
-        palabras strings;
-        listas enteros;
+        vector<T> datos;
 
     public:
-        JuntarDatos(); // constructor que inicializa los nombres
+        void agregar(const T& valor){
+            datos.push_back(valor);
+        }
 
-        template<typename T>
-        void agregar(const T& valor); // metodo template para agregar valores double o string
+        string procesar() const{
+            string resultado;
 
-        // metodo para agregar listas de enteros
-        void agregarLista(const vector<int>& fila);
+            // vec_doubles
+            if constexpr(is_same_v<T, double>){
+                resultado += "["
+                for (size_t i = 0; i < datos.size(); i++){
+                    resultado += to_string(datos[i]); // imprimo cada numero del vector
+                    if (i != datos.size() - 1) resultado += ", "; // imprimo coma si no es el último
+                }
+                resultado += "],\n ";
+            }
 
-        // getters
-        const vecDoubles& getVecDoubles() const;
-        const palabras& getPalabras() const;
-        const listas& getListas() const;
+            // palabras
+            else if constexpr(is_same_v<T, string>){
+                resultado += "["
+                for (size_t i = 0; i < datos.size(); i++){
+                    resultado += "\"" + datos[i] + "\"";
+                    if (i != datos.size() - 1) resultado += ", ";
+                }
+                resultado += "],\n ";
+            }
+
+            // listas
+            else if constexpr(is_same_v<T, string>){
+                resultado += "[\n";
+                for (size_t i = 0; i < datos.size(); i++){
+                    resultado += "       [";
+                    for (size_t j = 0; j < datos[i].size(); j++){
+                        resultado += to_string(datos[i][j]);
+                        if (j != datos[i].size() - 1) cout << ", "; // imprimo coma si no es el último
+                    }
+                    resultado += "]"
+                    if (i != datos.size() - 1) cout << ",";
+                    resultado += "\n";
+                }
+                resultado += "      ]\n";
+                resultado += "}\n";
+            }
+
+            else {
+                resultado = "Tipo no soportado";
+            }
+
+            return resultado;
+        }
 };
 
 // =========================== Clase 2 ===========================
 
 class ConstruirJson {
     private:
-        const JuntarDatos& datos;
+        string doublesStr;
+        string palabrasStr;
+        string listasStr;
 
     public:
-        ConstruirJson(const JuntarDatos& juntarDatos);
+        void setVecDoubles(const string& vec);
+        void setPalabras(const string& vec);
+        void setListas(const string& vec);
 
         void imprimirJson() const;
 };
--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -4,23 +4,32 @@ int main(){
     cout << "-------------------------------------------------" << "\n" << endl;
     cout << "Visualizacion del JSON:\n";
     
-    JuntarDatos colectorDatos;
+    JuntarDatos<double> datosDoubles;
 
     // agrego doubles
-    colectorDatos.agregar(1.3);
-    colectorDatos.agregar(2.1);
-    colectorDatos.agregar(3.2);
+    datosDoubles.agregar(1.3);
+    datosDoubles.agregar(2.1);
+    datosDoubles.agregar(3.2);
+
+    JuntarDatos<string> datosStrings;
 
     // agrego strings
-    colectorDatos.agregar(string("Hola"));
-    colectorDatos.agregar(string("Mundo"));
+    datosStrings.agregar("Hola");
+    datosStrings.agregar("Mundo");
+
+    JuntarDatos<vector<int>> datosListas;
 
     // agrego listas de enteros
-    colectorDatos.agregarLista({1, 2});
-    colectorDatos.agregarLista({3, 4});
+    datosListas.agregar({1, 2});
+    datosListas.agregar({3, 4});
 
     // creo el constructor de JSON
-    ConstruirJson constructor(colectorDatos);
+    ConstruirJson constructor;
+
+    constructor.setVecDoubles(datosDoubles.procesar());
+    constructor.setPalabras(datosStrings.procesar());
+    constructor.setListas(datosListas.procesar());
+    
     constructor.imprimirJson();
 
     cout << "\n-------------------------------------------------";
