e515b2d973b2928d70e1afd21791925863511c60
Isabella Cassanello <icassanello@udesa.edu.ar>
2025-05-28 23:17:18 -0300

    .

--- ejercicio1/sources/main.cpp
+++ ejercicio1/sources/main.cpp
@@ -7,35 +7,43 @@
 int main(){
     cout << "--------------------------------------------------------" << "\n" << endl;
 
-    // creo las mediciones
-    Posicion posicion(-34.6f, -58.4f, 950.0f, 5.3);
-    Presion presion(101.3f, 5.8f, 6.1f);
+    // =================== CREACIÓN DE LAS MEDICIONES ===================
 
-    // creo el objeto de SaveFlightData
-    SaveFlightData datosOriginales(posicion, presion);
+    Posicion posicion(-34.6f, -58.4f, 950.0f, 5.3); // creo un objeto de tipo Posicion con latitud, longitud, altitud y tiempo
+    Presion presion(101.3f, 5.8f, 6.1f);  // creo un objeto de tipo Presion con presion estatica, presion dinamica y tiempo
 
+    // =================== GUARDADO DE DATOS ===================
+
+    SaveFlightData datosOriginales(posicion, presion); // creo un objeto SaveFlightData con las dos mediciones
+
+    // abro un archivo binario para guardar los datos
     ofstream archivoSalida("vuelo.dat", ios::binary);
     if (!archivoSalida) {
         cerr << "Error al abrir archivo para escribir\n";
         return 1;
     }
 
+    // serializo los datos al archivo
     datosOriginales.serializar(archivoSalida);
     archivoSalida.close();
 
-    // leo los datos desde el archivo en otro objeto
-    SaveFlightData datosLeidos;
+    // =================== LECTURA DE DATOS DESDE ARCHIVO ===================
 
+    SaveFlightData datosLeidos; // creo un nuevo objeto para cargar los datos desde el archivo
+
+    // abro el archivo binario para lectura
     ifstream archivoEntrada("vuelo.dat", ios::binary);
     if (!archivoEntrada) {
         cerr << "Error al abrir archivo para leer.\n";
         return 1;
     }
 
+    // seserializo los datos y los cargo en el nuevo objeto
     datosLeidos.deserializar(archivoEntrada);
     archivoEntrada.close();
 
-    // muestro los datos recuperados
+    // =================== IMPRESIÓN DE LOS DATOS RECUPERADOS ===================
+
     cout << "============ Datos Recuperados del Archivo ============\n" << endl;
     datosLeidos.imprimir();
 
